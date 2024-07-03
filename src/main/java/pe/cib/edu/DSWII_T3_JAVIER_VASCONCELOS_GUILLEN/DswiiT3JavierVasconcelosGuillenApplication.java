e9a43adf6efdba48973f61ee60ab64c9c820523f
JavierSysAdmin <javier_30_01@hotmail.com>
2024-07-03 06:55:00 -0500

    creando el proyecto

---
+++ src/main/java/pe/cib/edu/DSWII_T3_JAVIER_VASCONCELOS_GUILLEN/DswiiT3JavierVasconcelosGuillenApplication.java
@@ -0,0 +1,13 @@
+package pe.cib.edu.DSWII_T3_JAVIER_VASCONCELOS_GUILLEN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3JavierVasconcelosGuillenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3JavierVasconcelosGuillenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/cib/edu/DSWII_T3_JAVIER_VASCONCELOS_GUILLEN/DswiiT3JavierVasconcelosGuillenApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.cib.edu.DSWII_T3_JAVIER_VASCONCELOS_GUILLEN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiT3JavierVasconcelosGuillenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
